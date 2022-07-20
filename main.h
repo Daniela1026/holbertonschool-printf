b48999f28457713a7a2b0e96349b35dbe3834257
Daniela1026 <cartagenad49@gmail.com>
2022-07-19 22:43:29 -0700

    format character string

--- main.h
+++ main.h
@@ -1,6 +1,23 @@
 #ifndef MAIN_H_
 #define MAIN_H_
+#include <stdarg.h>
 
+/**
+* charact - format output
+* Return: 0
+*/
+
+typedef struct charact
+{
+        char *charact;
+        int (*c)(va_list);
+} charact_t;
+
+
+int _putchar(char c);
 int _printf(const char *format, ...);
+
+int charr(va_list list);
+int str(va_list list);
 
 #endif
--- printf.c
+++ printf.c
@@ -1,27 +1,78 @@
 #include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stddef.h>
 #include "main.h"
 
 /**
-* *create_array -create array of chars
-* @size: Character int
-* @c: Character char
+* _printf - format is a character string
+* @format: Character char
 * Return: 0
 */
+
+int (*form(const char charact))(va_list)
+{
+	int l = 0;
+
+	charact_t ch[] = {
+                {"c", charr},
+                {"s", str},
+                {NULL, NULL}
+        };
+
+	while (l < 3)
+	{
+		if (charact == ch[l].charact[0])
+		{
+			return (ch[l].c);
+		}
+		l++;
+	}
+	return(NULL);
+}
+
+/**
+* _printf - format is a character string
+* @format: Character char
+* Return: 0
+*/
+
 int _printf(const char *format, ...)
 {
-	va_list ch;
-	unsigned int i = 0;
+	va_list list;
+	int n = 0, a = 0 ;
+	int (*i)();
 
-	charac_t print[] = {
-		{"c", char},
-		{"s", str},
-		{NULL, NULL}
-	};
+	va_start(list, format);
 
-	if (format == NULL)
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
-	va_start(ch, format);
-	i = printoperation(format, print, ch);
-	va_end(ch);
-	return(i);
+
+	while (format[n])
+	{
+		if (format[n] == '%' && format[n + 1] != '%')
+		{
+			i = form(format[n + 1]);
+			if (i == NULL)
+			{
+				_putchar(format[n]);
+				a++;
+				n++;
+			}
+			else
+			{
+				a += i(list);
+				n += 2;
+				continue;
+			}
+		}
+		else
+		{
+			_putchar(format[n]);
+			a++;
+			n++;
+		}
+	}
+	va_end(list);
+	return (a);
 }
