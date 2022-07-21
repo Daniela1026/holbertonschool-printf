a2a0e7a9859ed35f0931814c0fc23c1857d46628
Daniela1026 <cartagenad49@gmail.com>
2022-07-20 20:53:48 -0700

    character

--- main.h
+++ main.h
@@ -5,30 +5,22 @@
 
 int _putchar(char c);
 int _printf(const char *format, ...);
-/**int (*frm(const char charact))(va_list);*/
 int (*frm(const char *charact, int x))(va_list);
 int pr_char(va_list list);
 int pr_str(va_list list);
 int pr_percent(va_list list);
 
 /**
-* struct charact - format output
-* @charact: Character char
+* struct chara - format output
+* @chara: Character char
 * @c: Character c
 * Return: 0
 */
-
-/** Codigo Daniela
- * typedef struct charact
-{
-	char *charact;
-	int (*c)(va_list);
-} charact_t;*/
 
 typedef struct chara
 {
 	char *chara;
 	int (*c)(va_list);
-}charact_t;
+} charact_t;
 
 #endif
--- printf.c
+++ printf.c
@@ -1,39 +1,31 @@
 #include <stdarg.h>
+#include <stdio.h>
 #include <stddef.h>
 #include "main.h"
 
 /**
 * frm - format is a character string
 * @charact: Character char
+* @x: Character int
 * Return: 0
 */
 
-/**int (*frm(const char *charact, int x)(va_list)  */
 int (*frm(const char *charact, int x))(va_list)
 {
-	int l = 0;
-	
-	charact_t ch[] = {
-		{"c", pr_char}, {"s", pr_str}, {"%", pr_percent}
-	};
+        int l = 0;
 
-/**	while (l < 3)
-	{
-		if (charact == (ch[l].c))
-		{
-			return (ch[l].c);
-		}
-		l++;
-	}
-	return (NULL); */
-	for (l = 0; ch[l].chara != NULL; l++)
-	{
-		if (ch[l].chara[0] == charact[x])
-		{
-			return(ch[l].c);
-		}
-	}
-	return(NULL);
+        charact_t ch[] = {
+                {"c", pr_char}, {"s", pr_str}, {"%", pr_percent}
+        };
+
+        for (l = 0; ch[l].chara != NULL; l++)
+        {
+                if (ch[l].chara[0] == charact[x])
+                {
+                        return (ch[l].c);
+                }
+        }
+        return (NULL);
 }
 
 /**
@@ -41,43 +33,43 @@ int (*frm(const char *charact, int x))(va_list)
 * @format: Character char
 * Return: 0
 */
-
-/** int _printf(const char *format, ...)
+/**
+int _printf(const char *format, ...)
 {
-	va_list list;
-	int n = 0, a = 0;
-	int (*i)();
+        va_list list;
+        int n = 0, a = 0;
+        int (*i)();
 
-	va_start(list, format);
+        va_start(list, format);
 
-	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
-		return (0);
+        if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+                return (0);
 
-	while (format[n])
-	{
-		if (format[n] == '%' && format[n + 1] != '%')
-		{
-			i = frm(format[n + 1]);
-			if (i == NULL)
-			{
-				_putchar(format[n]);
-				a++;
-				n++;
-			}
-			else
-			{
-				a += i(list);
-				n += 2;
-				continue;
-			}
-		}
-		else
-		{
-			_putchar(format[n]);
-			a++;
-			n++;
-		}
-	}
-	va_end(list);
-	return (a);
+        while (format[n])
+        {
+                if (format[n] == '%' && format[n + 1] != '\0')
+                {
+                        i = frm(format[n + 1]);
+                        if (i == NULL)
+                        {
+                                _putchar(format[n]);
+                                a++;
+                                n++;
+                        }
+                        else
+                        {
+                                a += i(list);
+                                n += 2;
+                                continue;
+                        }
+                }
+                else
+                {
+                        _putchar(format[n]);
+                        a++;
+                        n++;
+                }
+        }
+        va_end(list);
+        return (a);
 }*/
